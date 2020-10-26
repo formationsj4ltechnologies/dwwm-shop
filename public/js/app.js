79da17870c87b6d060d05ecc51aed12ff9d05d13
J4LPROJECTS <gitprojects@j4ltechnologies.com>
2020-10-26 16:32:52 +0100

    commit implementation du panier

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,41 @@
-$(document).ready(function (e) {
-    $('#navbarDropdown').mouseenter(function (e) {
+$(document).ready(function () {
+    $('#navbarDropdown').mouseover(function (e) {
         e.preventDefault();
         $.ajax({
             url: "/panier/",
             method: "GET"
-        }).done(function (produits) {
-            // alert(produits.panier);
-            const imagePath = "images/produits"
+        }).done((articles) => {
+            //console.log(articles.panier);
+            const imagePath = "/images/produits"
             $('.contenu-panier').empty();
-            produits.forEach(function (article) {
-                console.log(article);
-                // $('.contenu-panier').append(
-                //     `<img src="${imagePath}/${article.produit.imageName}" alt="">`
-                // )
+            articles.panier.forEach(function (article) {
+                // console.log(article.produit.imageName);
+                $('.contenu-panier').append(
+                    `<tr>
+                        <td class="article-produit-image">
+                            <img src="${imagePath}/${article.produit.imageName}" alt="" class="img-fluid">
+                        </td>
+                        <td class="article-produit-nom-prix">
+                            <span class="text-lg-center text-uppercase"><b>${article.produit.nom}</b></span>                            
+                            <br>
+                            <span class="prix">${article.produit.prix} €</span>                            
+                        </td>
+                        <td class="article-produit-quantite">
+                            <i class="fas fa-arrow-circle-up augmenter"></i>
+                            ${article.quantite}
+                            <i class="fas fa-arrow-circle-down diminuer"></i>
+                        </td>    
+                        <td class="article-produit-supprimer">
+                            <i class="fas fa-trash"></i>
+                        </td>                    
+                    </tr>                                             
+                    `
+                )
             })
         });
-    })
+
+        $('.panier').click(function () {
+            $(this).toggle();
+        });
+    });
 });
